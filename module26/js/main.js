88f2e8b49d84051fdf502dcfcd2bcfdfd6041a37
reinazzee-lgtm <reinazzee@gmail.com>
2026-06-11 17:47:49 +0200

    commit

---
+++ module26/js/main.js
@@ -0,0 +1,52 @@
+console.log("hello from reina")
+
+var x=5;
+
+
+var y=28+43;
+console.log(y);
+
+var b=64%21;
+console.log(b);
+
+var c=99
+c++;
+console.log(c);
+
+var c=99
+c--;
+console.log(c);
+
+var q=4;
+var p=q**2
+console.log(p);
+
+var b=6;
+b*=2;
+console.log(b)
+
+var b=6;
+var a=7;
+console.log(a==b)
+
+var b=7;
+var a="7";
+console.log(a==b)
+
+var b=6;
+var a=7;
+console.log(a<b)
+
+var b=6;
+var a=7;
+console.log(a>b)
+
+var b=6;
+var a=74;
+console.log(a!=b)
+
+var name="reina"
+var lastname="zejnullahu"
+var fullname=name+ " " + lastname;
+console.log(fullname);
+
